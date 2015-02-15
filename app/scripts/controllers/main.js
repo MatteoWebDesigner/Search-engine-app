2dca1753bbe9149916e1d7b70917317691b3a700
MatteoWebDesigner <info@matteowebdesigner.com>
2015-02-15 00:26:47 +0000

    cleaning app, added some html and some css.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name searchEngineAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the searchEngineAppApp
+ */
+angular
+	.module('searchEngineApp')
+	.controller('MainCtrl', function ($scope) {
+
+	});
